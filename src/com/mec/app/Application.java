f9cb101349caca535809c31c84c28997ec18afc2
Paulo <paulituvitu@hotmail.com>
2020-02-11 12:15:46 -0300

    Image rendering

--- src/com/mec/app/Application.java
+++ src/com/mec/app/Application.java
@@ -5,25 +5,39 @@ import java.awt.event.KeyEvent;
 import com.mec.engine.GameContainer;
 import com.mec.engine.MecEngineApp;
 import com.mec.engine.Renderer;
+import com.mec.engine.gfx.Image;
 
 /** Represents a game using the engine. */
 public class Application extends MecEngineApp 
 {
+    private Image image;
+    private int imgPosX, imgPosY;
+
     public Application()
     {
+        image = new Image("/res/Placeholder.png");
     }
 
     @Override
     public void render(GameContainer gc, Renderer r) 
     {
         // TODO: Write game code here.
+
+        //Demo
+        r.drawImage(image, gc.getInput().getMouseX(), gc.getInput().getMouseY());
+        r.drawImage(image, imgPosX, imgPosY);
     }
 
     @Override
     public void update(GameContainer gc, float deltaTime) 
     {
-         // TODO: Write game code here.
-        if(gc.getInput().isKeyPressed(KeyEvent.VK_A)) System.out.println("Key A Pressed!");
+        // TODO: Write game code here.
+
+        //Demo
+        if(gc.getInput().isKeyPressed(KeyEvent.VK_A)) imgPosX -= 2;
+        if(gc.getInput().isKeyPressed(KeyEvent.VK_D)) imgPosX += 2;
+        if(gc.getInput().isKeyPressed(KeyEvent.VK_W)) imgPosY -= 2;
+        if(gc.getInput().isKeyPressed(KeyEvent.VK_S)) imgPosY += 2;
     }
 
     public static void main(String args[])
--- src/com/mec/engine/Renderer.java
+++ src/com/mec/engine/Renderer.java
@@ -2,6 +2,8 @@ package com.mec.engine;
 
 import java.awt.image.DataBufferInt;
 
+import com.mec.engine.gfx.Image;
+
 public class Renderer
 {
     private int pW, pH; //Pixel Width/Height
@@ -20,6 +22,24 @@ public class Renderer
         for(int i = 0; i < p.length; i++)
         {
             p[i] = 0xff222222;
+        }
+    }
+
+    public void setPixel(int x, int y, int value)
+    {
+        if( (x < 0 || x >= pW || y < 0 || y >= pH) || value == 0xffff00ff ) return;
+        
+        p[x + y * pW] = value;
+    }
+
+    public void drawImage(Image image, int offsetX, int offsetY)
+    {
+        for(int y = 0; y < image.getHeight(); y++)
+        {
+            for(int x = 0; x < image.getWidth(); x++)
+            {
+                setPixel(x + offsetX, y + offsetY, image.getPixels()[x + y * image.getWidth()]);
+            }
         }
     }
 }
