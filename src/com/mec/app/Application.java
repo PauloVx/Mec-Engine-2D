9b515d4c6770bceea050097713aa8149aca7d7b4
Paulo <paulituvitu@hotmail.com>
2020-02-12 17:31:16 -0300

    Audio

--- src/com/mec/app/Application.java
+++ src/com/mec/app/Application.java
@@ -9,16 +9,21 @@ import com.mec.engine.gfx.Font;
 import com.mec.engine.gfx.Image;
 import com.mec.engine.gfx.ImageTile;
 
+import com.mec.engine.audio.SoundClip;;
+
 /** Represents a game using the engine. */
 public class Application extends MecEngineApp 
 {
     private ImageTile explosionDemo;
     private int imgPosX, imgPosY;
 
+    private SoundClip sound;
+
     public Application()
     {
-        //image = new Image("/res/fire.gif");
         explosionDemo = new ImageTile("/res/fire_anim.mectexture", 32, 40);
+
+        sound = new SoundClip("/res/Audio/figure_it_out.wav");
     }
 
     @Override
@@ -27,8 +32,7 @@ public class Application extends MecEngineApp
         // TODO: Write game code here.
 
         //Demo
-        //r.drawImage(image, imgPosX + 256, imgPosY + 256);
-
+        r.drawText("TEXT RENDERING TEST", 0, 16, 0xffff0000);
         r.drawImageTile(explosionDemo, gc.getInput().getMouseX() - 8, gc.getInput().getMouseY() - 16, (int)tileX, 5);
         r.drawImageTile(explosionDemo, imgPosX, imgPosY, (int)tileX, 5);
     }
@@ -37,6 +41,9 @@ public class Application extends MecEngineApp
     public void update(GameContainer gc, float deltaTime) 
     {
         // TODO: Write game code here.
+        if(gc.getInput().isKeyPressed(KeyEvent.VK_P)) sound.playAudio();
+        if(gc.getInput().isKeyPressed(KeyEvent.VK_O)) sound.setVolume(+6);
+        if(gc.getInput().isKeyPressed(KeyEvent.VK_L)) sound.setVolume(-6);
 
         //Demo
         if(gc.getInput().isKeyPressed(KeyEvent.VK_A)) imgPosX -= 2;
